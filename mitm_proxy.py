d11f43382e4ea1bf312c3d0dc6bca63d33ed99a6
Hien Phan <hien.phan@evizi.com>
2019-07-25 09:18:49 +0700

    Add shutdown method

--- mitm_proxy.py
+++ mitm_proxy.py
@@ -1,3 +1,4 @@
+import asyncio
 from mitmproxy import proxy, options, ctx
 from mitmproxy.tools.dump import DumpMaster
 from mitmproxy import http
@@ -5,23 +6,32 @@ from mitmproxy import http
 
 class AdjustBody:
     def response(self, flow: http.HTTPFlow) -> None:
-        if "api/v5" in flow.request.url:
+        if "apiqa.vungle.com/api/v5/ads" in flow.request.url:
             print("Before intercept: %s" % flow.response.text)
             flow.response.content = bytes("This is replacement response", "UTF-8")
             print("After intercept: %s" % flow.response.text)
+            asyncio.ensure_future(stop())
 
 
 def start():
-    myaddon = AdjustBody()
+    add_on = AdjustBody()
+
     opts = options.Options(listen_host='192.168.1.224', listen_port=8888, confdir="/Users/hienphan/.mitmproxy")
-    pconf = proxy.config.ProxyConfig(opts)
-    m = DumpMaster(opts)
-    m.server = proxy.server.ProxyServer(pconf)
-    m.addons.add(myaddon)
+    proxy_conf = proxy.config.ProxyConfig(opts)
+
+    dump_master = DumpMaster(opts)
+    dump_master.server = proxy.server.ProxyServer(proxy_conf)
+    dump_master.addons.add(add_on)
+
     try:
-        m.run()
+        dump_master.run()
     except KeyboardInterrupt:
-        m.shutdown()
+        dump_master.shutdown()
 
 
-start()
+async def stop():
+    await asyncio.sleep(5)
+    print("shutting down...")
+    ctx.master.shutdown()
+
+
